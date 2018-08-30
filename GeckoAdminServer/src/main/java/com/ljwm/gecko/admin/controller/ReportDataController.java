16069eb6816b4fda71dfa4739208898acab1be9a
xxl <657824720@qq.com>
2018-08-30 09:34:36 +0800

    admin

--- GeckoAdminServer/src/main/java/com/ljwm/gecko/admin/controller/ReportDataController.java
+++ GeckoAdminServer/src/main/java/com/ljwm/gecko/admin/controller/ReportDataController.java
@@ -11,6 +11,7 @@ import com.ljwm.gecko.base.model.form.OtherReduceForm;
 import com.ljwm.gecko.base.model.form.OtherReduceQuery;
 import com.ljwm.gecko.base.model.vo.IncomeTypeSimpleVO;
 import com.ljwm.gecko.base.model.vo.IncomeTypeVO;
+import com.ljwm.gecko.base.model.vo.OtherReduceVo;
 import com.ljwm.gecko.base.model.vo.SpecialDeductionVO;
 import com.ljwm.gecko.base.service.IncomeTypeService;
 import com.ljwm.gecko.base.service.OtherReduceService;
@@ -38,56 +39,62 @@ public class ReportDataController extends BaseController {
 
   @GetMapping("findIncomeType")
   @ApiOperation(value = "查询收入类型列表")
-  public Result<List<IncomeTypeVO>> findIncomeType(){
+  public Result<List<IncomeTypeVO>> findIncomeType() {
     return success(incomeTypeService.find());
   }
 
   @PostMapping("saveIncomeType")
-  @ApiOperation(value ="保存收入类型" )
-  public Result<IncomeTypeSimpleVO> saveIncomeType(@RequestBody IncomeTypeDto incomeTypeDto){
+  @ApiOperation(value = "保存收入类型")
+  public Result<IncomeTypeSimpleVO> saveIncomeType(@RequestBody IncomeTypeDto incomeTypeDto) {
     return success(incomeTypeService.save(incomeTypeDto));
   }
 
   @GetMapping("deleteIncomeType/{id}")
   @ApiOperation(value = "根据id删除收入类型")
-  public Result deleteIncomeType(@PathVariable Long id){
+  public Result deleteIncomeType(@PathVariable Long id) {
     incomeTypeService.delete(id);
     return success();
   }
 
   @PostMapping("saveSpecialDeduction")
   @ApiOperation(value = "专项扣除添加")
-  public Result<SpecialDeductionVO> saveSpecialDeduction(@RequestBody SpecialDeductionDto specialDeductionDto){
+  public Result<SpecialDeductionVO> saveSpecialDeduction(@RequestBody SpecialDeductionDto specialDeductionDto) {
     return success(specialDeductionService.save(specialDeductionDto));
   }
 
   @GetMapping("findSpecialDeduction")
   @ApiOperation(value = "查询专项扣除---不带分页")
-  public Result<List<SpecialDeductionVO>> findSpecialDeduction(){
+  public Result<List<SpecialDeductionVO>> findSpecialDeduction() {
     return success(specialDeductionService.find());
   }
 
   @PostMapping("findSpecialDeductionPage")
   @ApiOperation(value = "查询专项扣除---带分页")
-  public Result<Page<SpecialDeductionVO>> findSpecialDeductionPage(@RequestBody SpecialDeductionQueryDto specialDeductionQueryDto){
+  public Result<Page<SpecialDeductionVO>> findSpecialDeductionPage(@RequestBody SpecialDeductionQueryDto specialDeductionQueryDto) {
     return success(specialDeductionService.findPage(specialDeductionQueryDto));
   }
 
   @PostMapping("findOtherReduce")
   @ApiOperation(value = "分页查询其他扣除")
-  public Result findOtherReduce(@RequestBody OtherReduceQuery query){
+  public Result<Page<OtherReduceVo>> findOtherReduce(@RequestBody OtherReduceQuery query) {
     return success(otherReduceService.find(query));
+  }
+
+  @PostMapping("getOtherReduce")
+  @ApiOperation("获取所有其他扣除")
+  public Result<List<OtherReduce>> getOtherReduce() {
+    return success(otherReduceService.getOtherReduce());
   }
 
   @PostMapping("saveOtherReduce")
   @ApiOperation(value = "其他扣除添加")
-  public Result<OtherReduce> saveOtherReduce(@RequestBody OtherReduceForm form){
+  public Result<OtherReduce> saveOtherReduce(@RequestBody OtherReduceForm form) {
     return success(otherReduceService.save(form));
   }
 
   @GetMapping("deleteOtherReduce/{id}")
   @ApiOperation(value = "根据id删除其他扣除")
-  public Result deleteOtherReduce(@PathVariable Long id){
+  public Result deleteOtherReduce(@PathVariable Long id) {
     otherReduceService.delete(id);
     return success();
   }
--- GeckoBase/src/main/java/com/ljwm/gecko/base/service/OtherReduceService.java
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/service/OtherReduceService.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -30,27 +31,43 @@ public class OtherReduceService {
   @Autowired
   private OtherReduceMapper otherReduceMapper;
 
+  /**
+   * 分页查询
+   *
+   * @param query
+   * @return
+   */
   public Page<OtherReduceVo> find(OtherReduceQuery query) {
     return commonService.find(query, (p, q) -> otherReduceMapper.find(p, BeanUtil.beanToMap(query)));
+  }
+
+  /**
+   * 非分页查询
+   *
+   * @return
+   */
+  public List<OtherReduce> getOtherReduce() {
+    return otherReduceMapper.selectList(null);
   }
 
   @Transactional
   public OtherReduce save(OtherReduceForm form) {
     return Optional.ofNullable(form).map(f -> {
-      OtherReduce otherReduce = new OtherReduce();
-      BeanUtil.copyProperties(f,otherReduce);
-      if (!Objects.isNull(f.getId())) {
-        otherReduceMapper.updateById(otherReduce);
-      }
-      otherReduceMapper.insert(otherReduce);
+      OtherReduce otherReduce = null;
+      if (!Objects.isNull(f.getId()))
+        otherReduce = otherReduceMapper.selectById(f.getId());
+      if (otherReduce == null)
+        otherReduce = new OtherReduce();
+      BeanUtil.copyProperties(f, otherReduce);
+      commonService.insertOrUpdate(otherReduce, otherReduceMapper);
       return otherReduce;
     }).get();
   }
 
   @Transactional
-  public void delete(Long id){
-    if (!otherReduceMapper.deleteAble(id)){
-      throw new LogicException(ResultEnum.DATA_ERROR,"节点已被使用,无法删除!");
+  public void delete(Long id) {
+    if (!otherReduceMapper.deleteAble(id)) {
+      throw new LogicException(ResultEnum.DATA_ERROR, "节点已被使用,无法删除!");
     }
     otherReduceMapper.deleteById(id);
   }
