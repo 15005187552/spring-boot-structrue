170bb9c5684859e7ac170c88ffc69a5037ebc7a3
Janiffy <fjdmjt@163.com>
2018-09-04 13:54:29 +0800

    a

--- GeckoClientServer/src/main/java/com/ljwm/gecko/client/service/CompanyService.java
+++ GeckoClientServer/src/main/java/com/ljwm/gecko/client/service/CompanyService.java
@@ -13,6 +13,7 @@ import com.ljwm.gecko.client.model.dto.CompanyForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
 
@@ -41,7 +42,15 @@ public class CompanyService {
     fileName, null, null, new Date(), null, companyForm.getProvCode(), companyForm.getCityCode(), companyForm.getAreaCode(), companyForm.getAddress());
     companyMapper.insert(company);
     return "提交成功！";
+  }
 
 
+  public Object upload(MultipartFile[] updateFiles) {
+    for (int i=0; i<updateFiles.length; i++) {
+      String fileName = FileKit.saveUploadFile(updateFiles[i], appInfo.getFilePath(), appInfo.getPic());
+      if (StrUtil.isEmpty(fileName))
+        return fail(ResultEnum.FAIL_TO_SAVE_FILE);
+    }
+    return "上传成功！";
   }
 }
