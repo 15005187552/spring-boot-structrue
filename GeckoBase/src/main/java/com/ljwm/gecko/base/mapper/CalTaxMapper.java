ad2cae3de844fbd0867011ba241e4ae3f0d28c96
Janiffy <fjdmjt@163.com>
2018-09-07 15:41:04 +0800

    push code

---
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/mapper/CalTaxMapper.java
@@ -0,0 +1,18 @@
+package com.ljwm.gecko.base.mapper;
+
+import com.ljwm.gecko.base.entity.CalTax;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 个税计算表 Mapper 接口
+ * </p>
+ *
+ * @author Levis
+ * @since 2018-09-07
+ */
+@Repository
+public interface CalTaxMapper extends BaseMapper<CalTax> {
+
+}
---
+++ GeckoClientServer/src/main/java/com/ljwm/gecko/client/dao/CalTaxDao.java
@@ -0,0 +1,30 @@
+package com.ljwm.gecko.client.dao;
+
+import com.ljwm.gecko.base.entity.CalTax;
+import com.ljwm.gecko.base.mapper.CalTaxMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Janiffy
+ * @date 2018/9/7 14:06
+ */
+@Repository
+public class CalTaxDao {
+
+  @Autowired
+  CalTaxMapper calTaxMapper;
+
+  public void insertOrUpdate(CalTax calTax, BigDecimal diff) {
+    CalTax cal = calTaxMapper.selectById(calTax.getGuestId());
+    if(cal != null){
+      if(diff.compareTo(BigDecimal.ZERO)>=0){
+        calTaxMapper.updateById(calTax);
+      }
+    } else {
+      calTaxMapper.insert(calTax);
+    }
+  }
+}
