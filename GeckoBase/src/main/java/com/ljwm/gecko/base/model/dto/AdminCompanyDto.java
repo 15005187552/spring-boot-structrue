65dd482ce6c0626de0b2e73d65a8f73d7f53e043
xxl <657824720@qq.com>
2018-09-08 13:51:18 +0800

    'all'

--- GeckoBase/src/main/java/com/ljwm/gecko/base/model/dto/AdminCompanyDto.java
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/model/dto/AdminCompanyDto.java
@@ -2,6 +2,7 @@ package com.ljwm.gecko.base.model.dto;
 
 import com.alibaba.fastjson.annotation.JSONField;
 import com.ljwm.gecko.base.config.CompanyEnumCodeToName;
+import com.ljwm.gecko.base.entity.Admin;
 import com.ljwm.gecko.base.entity.Company;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -9,6 +10,8 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class AdminCompanyDto extends Company {
+
+  private Admin validator;
 
   private String location;
 
