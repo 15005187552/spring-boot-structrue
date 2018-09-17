d5a64fe32ec427d72a35a3a9c33ceed2e0560ebc
yany <lj-yy@189china.net>
2018-09-17 14:28:32 +0800

    代码提交

--- GeckoAdminServer/src/main/java/com/ljwm/gecko/admin/controller/OrderController.java
+++ GeckoAdminServer/src/main/java/com/ljwm/gecko/admin/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.ljwm.gecko.admin.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ljwm.bootbase.controller.BaseController;
+import com.ljwm.bootbase.dto.Result;
+import com.ljwm.gecko.base.model.vo.OrderVo;
 import com.ljwm.gecko.base.service.OrderService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,9 @@ public class OrderController extends BaseController {
   @Autowired
   private OrderService orderService;
 
-  public
+  @PostMapping("find")
+  @ApiOperation("查询订单列表")
+  public Result<Page<OrderVo>>  find(){
+    return success();
+  }
 }
