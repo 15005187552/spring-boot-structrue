b5e4c4e1949154d0e60a26f0934a557fdc3bd393
xxl <lj-xxl@189china.net>
2018-09-28 20:26:54 +0800

    代码提交

--- GeckoProviderServer/src/main/java/com/ljwm/gecko/provider/service/ProviderMemberService.java
+++ GeckoProviderServer/src/main/java/com/ljwm/gecko/provider/service/ProviderMemberService.java
@@ -3,12 +3,14 @@ package com.ljwm.gecko.provider.service;
 import com.ljwm.gecko.base.mapper.MemberMapper;
 import com.ljwm.gecko.base.model.vo.MemberVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ProviderMemberService {
 
+  @Autowired
   private MemberMapper memberMapper;
 
   public MemberVo findMemberById(Long memberId){
