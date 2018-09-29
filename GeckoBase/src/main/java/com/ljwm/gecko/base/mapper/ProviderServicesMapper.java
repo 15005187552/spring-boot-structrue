51f5fcd40ad20467a25e6593e7aa37a0eba95e10
yany <lj-yy@189china.net>
2018-09-29 18:02:35 +0800

    代码提交

--- GeckoBase/src/main/java/com/ljwm/gecko/base/mapper/ProviderServicesMapper.java
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/mapper/ProviderServicesMapper.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ljwm.gecko.base.model.vo.ProviderServicesSimpleVo;
 import com.ljwm.gecko.base.model.vo.ProviderServicesVo;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -22,4 +23,7 @@ public interface ProviderServicesMapper extends BaseMapper<ProviderServices> {
   List<ProviderServicesSimpleVo> findClienProviderServicesListByProviderId(@Param("providerId") Long providerId);
 
   List<ProviderServicesVo> findProviderServicesDetailVoListByProviderId(@Param("providerId") Long providerId);
+
+  @Update("UPDATE t_provider_services SET STATUS=IF(STATUS=1,0,1) WHERE ID=#{id}")
+  int disabled(@Param("id") Long id);
 }
--- GeckoProviderServer/src/main/java/com/ljwm/gecko/provider/service/ProviderProviderService.java
+++ GeckoProviderServer/src/main/java/com/ljwm/gecko/provider/service/ProviderProviderService.java
@@ -1,10 +1,12 @@
 package com.ljwm.gecko.provider.service;
 
 import com.ljwm.gecko.base.mapper.ProviderMapper;
+import com.ljwm.gecko.base.mapper.ProviderServicesMapper;
 import com.ljwm.gecko.base.model.vo.ProviderVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,11 +17,19 @@ public class ProviderProviderService {
   @Autowired
   private ProviderMapper providerMapper;
 
+  @Autowired
+  private ProviderServicesMapper providerServicesMapper;
+
   public List<ProviderVo> findProviderListByMemberId(Long memberId){
     return providerMapper.findProviderListByMemberId(memberId);
   }
 
   public ProviderVo findProviderByProviderId(Long providerId){
     return providerMapper.findProviderByProviderId(providerId);
+  }
+
+  @Transactional
+  public Boolean disabled(Long id){
+    return providerServicesMapper.disabled(id)>=1;
   }
 }
