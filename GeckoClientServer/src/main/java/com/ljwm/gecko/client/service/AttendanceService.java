d467372fb10a3f31a6063d0d9de7cc896622947e
xxl <lj-xxl@189china.net>
2018-10-12 18:18:06 +0800

    add

--- GeckoClientServer/src/main/java/com/ljwm/gecko/client/service/AttendanceService.java
+++ GeckoClientServer/src/main/java/com/ljwm/gecko/client/service/AttendanceService.java
@@ -189,11 +189,11 @@ public class AttendanceService {
       TaxSpecialAdd taxSpecialAdd = taxSpecialAddMapper.selectOne(new QueryWrapper<TaxSpecialAdd>()
         .eq(TaxSpecialAdd.TAX_ID, tax.getId()).eq(TaxSpecialAdd.SPECIAL_ADD_ID, itemId));
       if (taxSpecialAdd != null){
-        taxSpecialAdd.setUpdateTime(date).setTaxMoney(new BigDecimal(value)).setUpdater(SecurityKit.currentId());
+        taxSpecialAdd.setUpdateTime(date).setTaxMoney(new BigDecimal(value)).setUpdater(SecurityKit.currentId()).setSpecialAddId(itemId);
         taxSpecialAddMapper.updateById(taxSpecialAdd);
       } else {
         taxSpecialAdd = new TaxSpecialAdd();
-        taxSpecialAdd.setUpdateTime(date).setTaxMoney(new BigDecimal(value)).setUpdater(SecurityKit.currentId()).setCreateTime(date);
+        taxSpecialAdd.setUpdateTime(date).setTaxMoney(new BigDecimal(value)).setUpdater(SecurityKit.currentId()).setCreateTime(date).setSpecialAddId(itemId);
         taxSpecialAddMapper.insert(taxSpecialAdd);
       }
     }
