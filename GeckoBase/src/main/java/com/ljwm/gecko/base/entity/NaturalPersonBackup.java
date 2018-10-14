486054da27e22eb27589e1f3458e1407a9262f4b
Janiffy <fjdmjt@163.com>
2018-10-14 13:17:54 +0800

    api

--- GeckoBase/src/main/java/com/ljwm/gecko/base/entity/NaturalPersonBackup.java
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/entity/NaturalPersonBackup.java
@@ -1,18 +1,17 @@
 package com.ljwm.gecko.base.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
-
-
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
