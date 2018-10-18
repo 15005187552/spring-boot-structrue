91dbbdf80c821a7b2cd1c344cc1b9f3bb0831c1d
xxl <657824720@qq.com>
2018-10-18 19:39:14 +0800

    'commit'

---
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/entity/Protocol.java
@@ -0,0 +1,70 @@
+package com.ljwm.gecko.base.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * <p>
+ * 系统自定义协议表
+ * </p>
+ *
+ * @author xixil
+ * @since 2018-10-18
+ */
+@Data
+@SuppressWarnings("ALL")
+@Accessors(chain = true)
+@TableName("`t_protocol`")
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "系统自定义协议表", subTypes = {Protocol.class})
+public class Protocol implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "`ID`", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "协议代码")
+    @TableField("`CODE`")
+    private String code;
+
+    @ApiModelProperty(value = "协议名称")
+    @TableField("`NAME`")
+    private String name;
+
+    @ApiModelProperty(value = "协议一句话描述")
+    @TableField("`DESCRIPTION`")
+    private String description;
+
+    @ApiModelProperty(value = "协议主题内容")
+    @TableField("`CONTENT`")
+    private String content;
+
+    @ApiModelProperty(value = "协议图片")
+    @TableField("`PIC_PATH`")
+    private String picPath;
+
+
+    public static final String ID = "`ID`";
+
+    public static final String CODE = "`CODE`";
+
+    public static final String NAME = "`NAME`";
+
+    public static final String DESCRIPTION = "`DESCRIPTION`";
+
+    public static final String CONTENT = "`CONTENT`";
+
+    public static final String PIC_PATH = "`PIC_PATH`";
+
+}
---
+++ GeckoBase/src/main/java/com/ljwm/gecko/base/mapper/ProtocolMapper.java
@@ -0,0 +1,16 @@
+package com.ljwm.gecko.base.mapper;
+
+import com.ljwm.gecko.base.entity.Protocol;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统自定义协议表 Mapper 接口
+ * </p>
+ *
+ * @author xixil
+ * @since 2018-10-18
+ */
+public interface ProtocolMapper extends BaseMapper<Protocol> {
+
+}
